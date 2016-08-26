c4c6ef2a3774b340ca67c6d02792d3b2f720a707
Dinesh V <dineshshabde@gmail.com>
2016-08-26 08:29:07 +0530

    Changed package name for class with main method Added test cases for service bean Added logger in service bean

--- src/main/java/org/example/ws/service/GreetingServiceBean.java
+++ src/main/java/org/example/ws/service/GreetingServiceBean.java
@@ -2,8 +2,13 @@ package org.example.ws.service;
 
 import java.util.Collection;
 
+import javax.persistence.EntityExistsException;
+import javax.persistence.NoResultException;
+
 import org.example.ws.model.Greeting;
 import org.example.ws.repository.GreetingRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
@@ -15,6 +20,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 public class GreetingServiceBean implements GreetingService {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	private GreetingRepository greetingRepository;
@@ -36,17 +43,20 @@ public class GreetingServiceBean implements GreetingService {
 	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
 	@CachePut(value = "greetings", key = "#result.id")
 	public Greeting create(Greeting greeting) {
+		logger.info("> create");
+
+		// Ensure the entity object to be created does NOT exist in the
+		// repository. Prevent the default behavior of save() which will update
+		// an existing entity if the entity matching the supplied id exists.
 		if (greeting.getId() != null) {
-			// save method of Spring JPA does both update and create.
-			// We cannot let create operation to update
-			return null;
+			// Cannot create Greeting with specified ID value
+			logger.error("Attempted to create a Greeting, but id attribute was not null.");
+			throw new EntityExistsException("The id attribute must be null to persist a new entity.");
 		}
+
 		Greeting savedGreeting = greetingRepository.save(greeting);
 
-		// Illustrate transaction rollback
-		if (savedGreeting.getId() == 4L) {
-			throw new RuntimeException("Roll me Back!");
-		}
+		logger.info("< create");
 		return savedGreeting;
 	}
 
@@ -54,12 +64,22 @@ public class GreetingServiceBean implements GreetingService {
 	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
 	@CachePut(value = "greetings", key = "#greeting.id")
 	public Greeting update(Greeting greeting) {
-		Greeting savedPersistance = findOne(greeting.getId());
-		if (savedPersistance == null) {
-			// Cannot update if id is not found in JPA repository
-			return null;
+		logger.info("> update id:{}", greeting.getId());
+
+		// Ensure the entity object to be updated exists in the repository to
+		// prevent the default behavior of save() which will persist a new
+		// entity if the entity matching the id does not exist
+		Greeting greetingToUpdate = findOne(greeting.getId());
+		if (greetingToUpdate == null) {
+			// Cannot update Greeting that hasn't been persisted
+			logger.error("Attempted to update a Greeting, but the entity does not exist.");
+			throw new NoResultException("Requested entity not found.");
 		}
-		Greeting updatedGreeting = greetingRepository.save(greeting);
+
+		greetingToUpdate.setText(greeting.getText());
+		Greeting updatedGreeting = greetingRepository.save(greetingToUpdate);
+
+		logger.info("< update id:{}", greeting.getId());
 		return updatedGreeting;
 	}
 
